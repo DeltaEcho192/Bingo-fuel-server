9d9996be8cf4b92ae8b03ed2124e128188a09095
DeltaEcho192 <anthony@durrer.co.za>
2023-06-18 15:07:56 +0000

    Basic turn around request for routes to test the front end:

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,32 @@ use axum::{
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
-
+use tower_http::{trace::TraceLayer};
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
+use http::{Request, Response, Method, header};
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any)
+        .allow_headers(Any);
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/users", post(create_user))
+        .route("/api/route", post(route))
+        .layer(cors);
 
     // run our app with hyper, listening globally on port 3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:3100".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();}
@@ -45,12 +56,37 @@ async fn create_user(
     (StatusCode::CREATED, Json(user))
 }
 
+async fn route(Json(payload): Json<Waypoints>,) -> (StatusCode, Json<Route>) {
+    let first = &payload.route[0];
+
+    println!("id: {}, location: {}",first.id, first.location);
+    let route = Route {
+        firstWaypoint: String::from(&first.location)
+    };
+
+    (StatusCode::CREATED, Json(route))
+}
 // the input to our `create_user` handler
 #[derive(Deserialize)]
 struct CreateUser {
     username: String,
 }
 
+#[derive(Deserialize)]
+struct Waypoint {
+    id: usize,
+    location: String
+}
+
+#[derive(Deserialize)]
+struct Waypoints {
+    route: Vec<Waypoint>,
+}
+
+#[derive(Serialize)]
+struct Route {
+    firstWaypoint: String,
+}
 // the output to our `create_user` handler
 #[derive(Serialize)]
 struct User {
